989bcf190d63a00a4447c7fcceb24f4ba9bdd666
Femi Fagbure <femifagbure@gmail.com>
2020-05-14 19:27:45 -0400

    created server

---
+++ server.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+const bodyParser = require('body-parser');
+const path = require('path');
+var express = require('express');
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+ 
+const port = process.env.PORT || 3001
+app.use('/', express.static('public'))
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+  }
+
+app.listen(port, function () {
+    console.log(`Listening on PORT: ${port}`)
+});
