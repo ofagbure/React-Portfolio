4aaa1ac297376a24c4b0e16f2c66d9314f16878e
Femi Fagbure <femifagbure@gmail.com>
2020-05-14 20:14:01 -0400

    more clean up

--- client/src/components/footer/index.js
+++ client/src/components/footer/index.js
@@ -12,7 +12,7 @@ function Footer() {
                         <span className="text-muted clickme" id="text">
                             <a href="https://www.linkedin.com/in/femi-fagbure/"><img className="icon"
                                 src="./images/linkedin.png" alt="..." /></a>
-                            <a href="https://github.com/ofagbure"><img className="icon" src="./images/github.png" alt="..." /></a>
+                            <a href="https://github.com/ofagbure"><img className="icon" src="./images/GitHub.png" alt="..." /></a>
                             <a href="mailto:femifagbure@gmail.com"><img className="icon" src="./images/email.png" alt="..." /></a>
                             <a href="tel:917-442-0383"><img className="icon" src="./images/phone.jpg" alt="..." /></a>
                             <p>Copyright &copy; 2020 Femi Fagbure&nbsp;• Georgia Institute of Technology&nbsp;</p>
